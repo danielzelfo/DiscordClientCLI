d3aa2a813c251be3d956ac93829c5c1a780c072e
Daniel Zelfo <daniel.zelfo@gmail.com>
2022-03-28 22:29:05 -0700

    Added optional command line argument to print the channel ID and message prompts

--- client.py
+++ client.py
@@ -4,16 +4,22 @@ import time
 import argparse
 
 client = discord.Client()
+printPrompts = False
 
 @client.event
 async def on_ready():
 	print(f'{client.user} has connected to Discord!')
 	
 	channel = None
+	channelPrompt =  ""
+	messagePrompt = ""
+	if printPrompts:
+		channelPrompt =  "Enter channel ID: "
+		messagePrompt = "Enter message: "
 	
 	while True:
-		line = input()
-		if channel is None: #  input channel
+		if channel is None: # input channel
+			line = input(channelPrompt)
 			if line == "":
 				break
 			
@@ -25,7 +31,8 @@ async def on_ready():
 			channel = client.get_channel(channelid)
 			if channel is None:
 				print(f"Invalid Channel ID: \"{line}\"", file=sys.stderr)
-		else:
+		else: # input message
+			line = input(messagePrompt)
 			if line == "":
 				channel = None
 				continue
@@ -40,6 +47,8 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("token", help="account token (user token by default)")
 	parser.add_argument("-b", "--bot", action="store_true", help="provided token is for a bot")
+	parser.add_argument("-p", "--print", action="store_true", help="print prompts")
 	args = parser.parse_args()
 	
+	printPrompts = args.print
 	client.run(args.token, bot=args.bot)
